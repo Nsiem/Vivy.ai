84926fd4c604b3144d4025c67aefcca9b5178706
Tony K <82545368+Nsiem@users.noreply.github.com>
2022-05-14 00:00:58 -0400

    basic bot up and running

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const Discord = require('discord.js')
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES );
+const client = new Client({ intents: myIntents });
+const dotenv = require('dotenv')
+dotenv.config()
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on("messageCreate", (message) => {
+    if (message.content == "!ping"){
+        message.channel.send("PONG")
+    } else {
+        return
+    }
+});
+
+client.login(process.env.TOKEN)
